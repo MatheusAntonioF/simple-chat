1f723f12ecfe29dda5cf155392fd29eb00a5301d
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2023-02-02 21:53:00 -0300

    feat: initial setup
    config webpack, babel, eslint and prettier

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const { resolve, join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ['babel-loader'],
+        exclude: /node-modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: join(__dirname, './src'),
+    port: 3000,
+    hot: 'only',
+    compress: true,
+    open: true,
+  },
+};
