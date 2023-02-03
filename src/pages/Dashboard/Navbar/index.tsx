67a9522b6953b621ddf6f1ce6b633af9ccaf17e3
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2023-02-02 23:56:48 -0300

    feat: create dashboard skeleton

---
+++ src/pages/Dashboard/Navbar/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Stack,
+  Center,
+  Heading,
+  Link,
+} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <>
+      <Box bg="gray.100" px={4}>
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <Heading size="md">Simple chat</Heading>
+
+          <Flex alignItems="center">
+            <Stack direction="row" spacing={7}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded="full"
+                  variant="link"
+                  cursor="pointer"
+                  minW={0}
+                >
+                  <Avatar
+                    size="sm"
+                    src="https://avatars.dicebear.com/api/male/username.svg"
+                  />
+                </MenuButton>
+                <MenuList alignItems="center">
+                  <br />
+                  <Center>
+                    <Avatar
+                      size="2xl"
+                      src="https://avatars.dicebear.com/api/male/username.svg"
+                    />
+                  </Center>
+
+                  <MenuDivider />
+                  <MenuItem>
+                    <Link as={RouterLink} w="full" to="/me">
+                      Profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,28 @@
 import React, { FC } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Login } from '../pages/Login';
+import { Dashboard } from '../pages/Dashboard';
+// import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
+import { UserProfile } from '../pages/UserProfile';
 
 export const Routes: FC = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Login />,
+      element: <Dashboard />, //<Login />,
     },
     {
       path: '/register',
       element: <Register />,
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />,
+    },
+    {
+      path: '/me',
+      element: <UserProfile />,
     },
   ]);
 
